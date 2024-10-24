478a4c610b902915e0f0083e8d468665ddbcf0b8
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-10-24 12:26:20 +0700

    enable fileMiddleware

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,16 +7,19 @@ import Vapor
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    app.routes.defaultMaxBodySize = "10mb"
+    
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     try app.databases.use(DatabaseConfigurationFactory.mongo(
         connectionString: Environment.get("DATABASE_URL") ?? "mongodb://localhost:27017/vapor_database"
     ), as: .mongo)
 
-    app.migrations.add(CreateTodo())
+    app.migrations.add(createTestModel())
+    try await app.autoMigrate()
 
     app.views.use(.leaf)
-
 
     // register routes
     try routes(app)
