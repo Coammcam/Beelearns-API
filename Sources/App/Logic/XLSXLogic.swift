8f8efbe83e340ecccdc373ef6943fe8b644eacdf
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-10-28 05:41:12 +0700

    Moved XLSXLogic.swift to logic folder and refactor

--- Sources/App/Logic/XLSXLogic.swift
+++ Sources/App/Logic/XLSXLogic.swift
@@ -6,24 +6,26 @@
 //
 
 import CoreXLSX
+import Fluent
+import Vapor
 
-func parseXLSX(pathToXLSX: String) throws -> [TestModel]{
+func parseXLSX<valueType>(_:valueType.Type, XLSXData: Data) throws -> [valueType]{
     
-    var returnObject: [TestModel] = []
-    let inXLSX = XLSXFile(filepath: pathToXLSX)
+    var returnObject: [valueType] = []
+    let inXLSX = try XLSXFile(data: XLSXData)
     
     var rowCount: Int = 0
     var columnAStrings: [String?] = []
     var columnBStrings: [String?] = []
     var columnCStrings: [String?] = []
     
-    for wbk in try inXLSX!.parseWorkbooks() {
-        for (_, path) in try inXLSX!.parseWorksheetPathsAndNames(workbook: wbk) {
-            let worksheet = try inXLSX!.parseWorksheet(at: path)
+    for wbk in try inXLSX.parseWorkbooks() {
+        for (_, path) in try inXLSX.parseWorksheetPathsAndNames(workbook: wbk) {
+            let worksheet = try inXLSX.parseWorksheet(at: path)
             
             rowCount = worksheet.data?.rows.count ?? [].count
             
-            if let sharedStrings = try inXLSX!.parseSharedStrings() {
+            if let sharedStrings = try inXLSX.parseSharedStrings() {
                 
                 columnAStrings = worksheet.cells(atColumns: [ColumnReference("A")!])
                     .map {columnCString in
@@ -46,9 +48,17 @@ func parseXLSX(pathToXLSX: String) throws -> [TestModel]{
         return returnObject
     }
     
-    for i in 0...rowCount-1{
-        if(columnAStrings[i] != nil && columnBStrings[i] != nil && columnCStrings[i] != nil){
-            returnObject.append(TestModel(value1: columnAStrings[i], value2: columnBStrings[i], value3: columnCStrings[i]))
+    if (valueType.self is Word.Type){
+        for i in 0...rowCount-1{
+            if(columnAStrings[i] != nil && columnBStrings[i] != nil){
+                returnObject.append(Word(englishWord: columnAStrings[i], vietnameseMeaning: columnBStrings[i]) as! valueType)
+            }
+        }
+    }else if(valueType.self is TestModel.Type){
+        for i in 0...rowCount-1{
+            if(columnAStrings[i] != nil && columnBStrings[i] != nil && columnCStrings[i] != nil){
+                returnObject.append(TestModel(value1: columnAStrings[i], value2: columnBStrings[i], value3: columnCStrings[i]) as! valueType)
+            }
         }
     }
     
