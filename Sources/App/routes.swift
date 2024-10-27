6677f2d21f5bd5605b35bc61608b99e68dea0f34
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-10-28 05:44:15 +0700

    Refactor "upload-data" route and clean up code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,60 +3,38 @@ import Vapor
 import CoreXLSX
 import Foundation
 
-struct testObject: Equatable{
-    var value1: String?
-    var value2: String?
-    var value3: String?
-}
-
 func routes(_ app: Application) throws {
     
     var statusMessage = ""
-    var testObjects: [TestModel] = []
-    
-    struct AdminPageData: Encodable{
-        var statusMessage = ""
-        var testObjects: [TestModel] = []
-    }
+//    struct AdminPageData: Encodable{
+//        var statusMessage = ""
+//        var testObjects = []
+//    }
     
     app.get("admin") { req async throws in
-        try await req.view.render("index", AdminPageData(statusMessage: statusMessage, testObjects: testObjects))
+//        try await req.view.render("index", AdminPageData(statusMessage: statusMessage, testObjects: testObjects))
+        try await req.view.render("index", ["statusMessage": statusMessage])
     }
     
     app.post("upload-data") { req throws in
         struct InputExcelSheet: Content{
             var file: File
         }
-
+        
+//        print(req.peerAddress?.hostname)
+//        print(req.peerAddress?.ipAddress)
+        
         let inputXlxs = try req.content.decode(InputExcelSheet.self)
-
+        
+        let xlsxData = Data(buffer: inputXlxs.file.data)
+        
         if(inputXlxs.file.extension != "xlsx"){
             statusMessage = "not an excel sheet"
             return req.redirect(to: "admin")
         }
 
-        let path = app.directory.publicDirectory + inputXlxs.file.filename
-
-        if (FileManager.default.fileExists(atPath: path)){
-            testObjects = try parseXLSX(pathToXLSX: path)
-            statusMessage = "done"
-            for testObject in testObjects {
-                _ = testObject.save(on: req.db)
-            }
-        }else{
-            _ = app.fileio.openFile(path: path, mode: .write, flags: .allowFileCreation(posixMode: 0x744), eventLoop: req.eventLoop)
-                .flatMap {handle in
-                    req.application.fileio.write(fileHandle: handle, buffer: inputXlxs.file.data, eventLoop: req.eventLoop)
-                        .flatMapThrowing { _ in
-                            try handle.close()
-                            statusMessage = "upload success"
-                            testObjects = try parseXLSX(pathToXLSX: path)
-                            for testObject in testObjects {
-                                _ = testObject.save(on: req.db)
-                            }
-                        }
-                }
-        }
+        _ = try parseXLSX(_: Word.self, XLSXData: xlsxData).create(on: req.db)
+        statusMessage = "done"
         return req.redirect(to: "admin")
     }
     
@@ -79,4 +57,5 @@ func routes(_ app: Application) throws {
     }
     
     try app.register(collection: TestModelController(app: app))
+    try app.register(collection: WordController())
 }
