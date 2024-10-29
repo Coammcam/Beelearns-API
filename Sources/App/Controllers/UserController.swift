81a0750bfd58a41583b13ea72bc5ff446032e1b5
Coammcam <hungnguyenisme84@gmail.com>
2024-10-30 03:23:30 +0700

    feat: api for user

---
+++ Sources/App/Controllers/UserController.swift
@@ -0,0 +1,62 @@
+//
+//  UserController.swift
+//
+//
+//  Created by Nguyễn Hưng on 20/10/2024.
+//
+
+import Vapor
+
+struct UserController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        let usersRoute = routes.grouped("user")
+        usersRoute.get(":id", use: getUser)
+        usersRoute.put(":id", use: updateUser)
+        usersRoute.delete(":id", use: deleteUser)
+    }
+    
+    func getUser(req: Request) throws -> EventLoopFuture<UserDTO> {
+        guard let userId = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest, reason: "Invalid user ID")
+        }
+        
+        return User.find(userId, on: req.db)
+            .unwrap(or: Abort(.notFound, reason: "User not found"))
+            .map { user in
+                return user.toDTO()
+            }
+    }
+    
+    func updateUser(req: Request) throws -> EventLoopFuture<User> {
+        guard let userId = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest, reason: "Invalid user ID")
+        }
+        
+        let updateData = try req.content.decode(UserDTO.self)
+        
+        return User.find(userId, on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { user in
+                user.username = updateData.username
+                user.phone_number = updateData.phone_number
+                user.profile_image = updateData.profile_image
+                user.date_of_birth = updateData.date_of_birth
+                return user.save(on: req.db).map { user }
+            }
+    }
+    
+    func deleteUser(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        guard let userId = req.parameters.get("id", as: UUID.self) else {
+            throw Abort(.badRequest, reason: "Invalid user ID")
+        }
+        
+        return User.find(userId, on: req.db)
+            .unwrap(or: Abort(.notFound, reason: "User not found"))
+            .flatMap { user in
+                return user.delete(on: req.db)
+            }
+            .transform(to: .noContent)
+    }
+
+
+}
---
+++ Sources/App/DTOs/UserDTO.swift
@@ -0,0 +1,32 @@
+//
+//  UserDTO.swift
+//
+//
+//  Created by Nguyễn Hưng on 20/10/2024.
+//
+
+import Vapor
+
+struct UserDTO: Content {
+    var id: UUID?
+    var email: String
+    var username: String
+//    var password: String
+    var phone_number: String?
+    var date_of_birth: String?
+    var profile_image: String?
+    
+    func toModel() -> User {
+        let model = User()
+        
+        model.id = self.id
+        model.username = self.username
+        model.email = self.email
+        model.phone_number = self.phone_number
+        model.date_of_birth = self.date_of_birth
+        model.profile_image = self.profile_image
+//        model.password = self.password
+        
+        return model
+    }
+}
