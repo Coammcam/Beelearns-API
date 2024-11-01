097c8cf217d345bebd43c5c23d771081bd7c7344
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-11-01 14:45:00 +0700

    Add true false question mode to XLSXLogic

--- Sources/App/Logic/XLSXLogic.swift
+++ Sources/App/Logic/XLSXLogic.swift
@@ -18,7 +18,8 @@ func parseXLSX<valueType>(_:valueType.Type, XLSXData: Data) throws -> [valueType
     var columnAStrings: [String?] = []
     var columnBStrings: [String?] = []
     var columnCStrings: [String?] = []
-    
+    var columnDStrings: [String?] = []
+
     for wbk in try inXLSX.parseWorkbooks() {
         for (_, path) in try inXLSX.parseWorksheetPathsAndNames(workbook: wbk) {
             let worksheet = try inXLSX.parseWorksheet(at: path)
@@ -28,18 +29,21 @@ func parseXLSX<valueType>(_:valueType.Type, XLSXData: Data) throws -> [valueType
             if let sharedStrings = try inXLSX.parseSharedStrings() {
                 
                 columnAStrings = worksheet.cells(atColumns: [ColumnReference("A")!])
-                    .map {columnCString in
-                        return columnCString.stringValue(sharedStrings)
+                    .map {columnAString in
+                        return columnAString.stringValue(sharedStrings)
                     }
                 columnBStrings = worksheet.cells(atColumns: [ColumnReference("B")!])
-                    .map {columnCString in
-                        return columnCString.stringValue(sharedStrings)
+                    .map {columnBString in
+                        return columnBString.stringValue(sharedStrings)
                     }
                 columnCStrings = worksheet.cells(atColumns: [ColumnReference("C")!])
                     .map {columnCString in
                         return columnCString.stringValue(sharedStrings)
                     }
-                
+                columnDStrings = worksheet.cells(atColumns: [ColumnReference("D")!])
+                    .map {columnDString in
+                        return columnDString.stringValue(sharedStrings)
+                    }
             }
         }
     }
@@ -54,10 +58,10 @@ func parseXLSX<valueType>(_:valueType.Type, XLSXData: Data) throws -> [valueType
                 returnObject.append(Word(englishWord: columnAStrings[i], vietnameseMeaning: columnBStrings[i]) as! valueType)
             }
         }
-    }else if(valueType.self is TestModel.Type){
+    }else if(valueType.self is TrueFalseQuestion.Type){
         for i in 0...rowCount-1{
-            if(columnAStrings[i] != nil && columnBStrings[i] != nil && columnCStrings[i] != nil){
-                returnObject.append(TestModel(value1: columnAStrings[i], value2: columnBStrings[i], value3: columnCStrings[i]) as! valueType)
+            if (columnAStrings[i] != nil && columnBStrings[i] != nil && columnCStrings[i] != nil){
+                returnObject.append(TrueFalseQuestion(content: columnAStrings[i]!, answer: columnCStrings[i]!, vietnameseMeaning: columnBStrings[i]!, correction: columnDStrings[i] ?? "", topic: "") as! valueType)
             }
         }
     }
