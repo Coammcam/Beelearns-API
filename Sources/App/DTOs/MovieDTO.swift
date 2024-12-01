127008535f9d5f20b569399b6a53d4809d43a2c0
Coammcam <hungnguyenisme84@gmail.com>
2024-12-01 17:33:38 +0700

    add "create, delete, update" api for Movie

--- Sources/App/DTOs/MovieDTO.swift
+++ Sources/App/DTOs/MovieDTO.swift
@@ -16,4 +16,19 @@ struct MovieDTO: Content{
     var genre: String?
     var rating: String?
     var year: String?
+    
+    func toModel() -> Movie {
+        let model = Movie()
+        
+        model.title = self.title ?? ""
+        model.banner = self.banner ?? ""
+        model.poster = self.poster ?? ""
+        model.description = self.description ?? ""
+        model.duration = self.duration ?? ""
+        model.genre = self.genre ?? ""
+        model.rating = self.rating ?? ""
+        model.year = self.year ?? ""
+        
+        return model
+    }
 }
