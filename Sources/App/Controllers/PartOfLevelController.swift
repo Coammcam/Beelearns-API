5a3e143b262442e2bc6d0687c12e9f7472f41910
Coammcam <hungnguyenisme84@gmail.com>
2024-12-27 15:29:54 +0700

    api add PartOfLevel

--- Sources/App/Controllers/PartOfLevelController.swift
+++ Sources/App/Controllers/PartOfLevelController.swift
@@ -18,13 +18,29 @@ struct PartOfLevelController: RouteCollection {
     func addPartOfLevel(req: Request) async throws -> PartOfLevelDTO {
         let partOfLevelDTO = try req.content.decode(PartOfLevelDTO.self)
         
-        guard let matchingLevel = try await Level.query(on: req.db)
+        guard let currentLevel = try await Level.query(on: req.db)
             .filter(\.$level == partOfLevelDTO.level_id)
-            .first(), let levelID = matchingLevel.id else {
+            .first(), let currentLevelID = currentLevel.id else {
             throw Abort(.notFound, reason: "Level not found for the provided difficulty.")
         }
         
-        let partOfLevel = PartOfLevel(id: nil, part: partOfLevelDTO.part, levelID: levelID)
+        if let previousLevelDifficulty = Level_difficulty(rawValue: partOfLevelDTO.level_id.rawValue - 1),
+           let previousLevel = try await Level.query(on: req.db)
+            .filter(\.$level == previousLevelDifficulty)
+            .first() {
+            
+            // Lấy part lớn nhất của Level trước đó
+            let maxPartInPreviousLevel = try await PartOfLevel.query(on: req.db)
+                .filter(\.$level.$id == previousLevel.id!)
+                .sort(\.$part, .descending)
+                .first()?.part ?? 0
+            
+            if partOfLevelDTO.part <= maxPartInPreviousLevel {
+                throw Abort(.badRequest, reason: "The part must be greater than the maximum part in the previous level.")
+            }
+        }
+        
+        let partOfLevel = PartOfLevel(id: nil, part: partOfLevelDTO.part, levelID: currentLevelID)
         try await partOfLevel.save(on: req.db)
         
         return try await partOfLevel.toDTO(req: req)
