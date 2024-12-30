fcdf21d739fbee7370317004081930ad0bf38692
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-12-30 14:39:38 +0700

    Create CreateGrammarQuestion.swift

---
+++ Sources/App/Migrations/CreateGrammarQuestion.swift
@@ -0,0 +1,28 @@
+//
+//  CreateGrammarQuestion.swift
+//  
+//
+//  Created by HoangDus on 27/12/2024.
+//
+
+import Fluent
+
+struct CreateGrammarQuestion: AsyncMigration{
+    func prepare(on database: FluentKit.Database) async throws {
+        try await database.schema("grammar-questions")
+            .id()
+            .field("question", .string, .required)
+            .field("content", .string, .required)
+            .field("meaning", .string, .required)
+            .field("topic", .string, .required)
+            .field("level", .string, .required)
+            .field("create_at", .date)
+            .field("update_at", .date)
+            .unique(on: "content")
+            .create()
+    }
+    
+    func revert(on database: Database) async throws {
+        try await database.schema("grammar-questions").delete()
+    }
+}
