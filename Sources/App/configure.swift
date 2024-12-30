543cdcf0628f6b4f7508db8db412eea7f4eec262
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-12-30 15:12:58 +0700

    Add migrations to configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,15 +16,15 @@ public func configure(_ app: Application) async throws {
         connectionString: Environment.get("DATABASE_URL")!
     ), as: .mongo)
 
-//    app.migrations.add(CreateWord())
-//    app.migrations.add(CreateTrueFalseQuestion())
-//    try await app.autoMigrate()
-    app.leaf.tags["dateFormat"] = DateFormatTag()
     app.migrations.add(CreateUser())
-    
     app.migrations.add(CreateLevel())
     app.migrations.add(CreatePartOfLevels())
-
+    app.migrations.add(CreateUserStudyData())
+    app.migrations.add(CreateWord())
+    app.migrations.add(CreateGrammarQuestion())
+    app.migrations.add(CreateTrueFalseQuestion())
+    
+    app.leaf.tags["dateFormat"] = DateFormatTag()
     app.views.use(.leaf)
 
     // register routes
