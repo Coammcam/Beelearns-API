9e02f2714f7027c79497a3fad1519776a351973a
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-12-30 14:54:13 +0700

    Update user model and DTO

--- Sources/App/DTOs/UserDTO.swift
+++ Sources/App/DTOs/UserDTO.swift
@@ -10,12 +10,9 @@ import Vapor
 struct UserDTO: Content {
     var email: String
     var username: String
-//    var password: String
     var phoneNumber: String?
     var dateOfBirth: String?
     var profileImageUrl: String?
-    var heart: Int?
-    var honey_jar: Int?
     
     func toModel() -> User {
         let model = User()
@@ -25,16 +22,16 @@ struct UserDTO: Content {
         model.phone_number = self.phoneNumber
         model.date_of_birth = self.dateOfBirth
         model.profile_image = self.profileImageUrl
-//        model.password = self.password
-        model.heart = self.heart
-        model.honey_jar = self.honey_jar
         return model
     }
 }
 
-struct UserCurrency: Content{
-    let honeyJar: Int
-    let honeyComb: Int
-    let level: Int
-}
 
+struct UserStudyDataDTO: Content{
+    var score: Int
+    var exp: Int
+    var part: Int
+    var level: Int
+    var heart: Int
+    var honeyJar: Int
+}
--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -9,7 +9,7 @@ import Fluent
 import Vapor
 import FluentMongoDriver
 
-final class User: Model, Content {
+final class User: Model {
     static let schema = "users"
     
     @ID(custom: .id)
@@ -33,15 +33,6 @@ final class User: Model, Content {
     @Field(key: "profile_image")
     var profile_image: String?
     
-    @Field(key: "heart")
-    var heart: Int?
-    
-    @Field(key: "honey_jar")
-    var honey_jar: Int?
-    
-    @OptionalField(key: "level")
-    var level: Int?
-    
     @Timestamp(key: "created_at", on: .create)
     var createdAt: Date?
     
@@ -49,8 +40,17 @@ final class User: Model, Content {
     var updatedAt: Date?
     
     init() {}
-    
-    init(id: ObjectId? = nil, email: String, username: String, password: String, phone_number: String? = nil, date_of_birth: String? = nil, profile_image: String? = nil, heart: Int? = 5, honey_jar: Int? = 100, level: Int? = 1, createdAt: Date? = nil, updatedAt: Date? = nil) {
+
+    init(id: ObjectId? = nil,
+         email: String,
+         username: String,
+         password: String,
+         phone_number: String? = nil,
+         date_of_birth: String? = nil,
+         profile_image: String? = nil,
+         createdAt: Date? = nil,
+         updatedAt: Date? = nil) {
+        
         self.id = id
         self.email = email
         self.username = username
@@ -58,9 +58,6 @@ final class User: Model, Content {
         self.phone_number = phone_number
         self.date_of_birth = date_of_birth
         self.profile_image = profile_image
-        self.heart = heart
-        self.honey_jar = honey_jar
-        self.level = level
         self.createdAt = createdAt
         self.updatedAt = updatedAt
     }
@@ -71,14 +68,8 @@ final class User: Model, Content {
             username: self.username,
             phoneNumber: self.phone_number ?? "",
             dateOfBirth: self.date_of_birth ?? "",
-            profileImageUrl: self.profile_image ?? "",
-            heart: self.heart,
-            honey_jar: self.honey_jar
+            profileImageUrl: self.profile_image ?? ""
         )
     }
-    
-    func toCurrencyData() -> UserCurrency{
-        return UserCurrency(honeyJar: honey_jar ?? 0, honeyComb: heart ?? 0, level: level ?? 1)
-    }
-    
 }
+
