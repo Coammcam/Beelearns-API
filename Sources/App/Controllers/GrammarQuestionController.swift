ab2dfcae5e44c56c31a7e3bf41155f81de75767d
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-01-10 17:34:49 +0700

    update get grammar by amount endpoint

--- Sources/App/Controllers/GrammarQuestionController.swift
+++ Sources/App/Controllers/GrammarQuestionController.swift
@@ -13,7 +13,7 @@ struct GrammarQuestionController: RouteCollection {
     func boot(routes: Vapor.RoutesBuilder) throws {
         let grammarquestions = routes.grouped("grammarquestions")
         
-        grammarquestions.get("byamount", use: getByAmount)
+        grammarquestions.get(use: getByAmount)
         
         grammarquestions.group(":id"){ grammar in
             grammar.put(use: updateByID)
@@ -25,18 +25,7 @@ struct GrammarQuestionController: RouteCollection {
     func getByAmount(req: Request) async throws -> [GrammarQuestionDTO] {
         guard var amount: Int = req.query["amount"] else { throw Abort(.badRequest) }
         
-        let count = try await GrammarQuestion.query(on: req.db).count()
-        
-        if(amount <= 0){
-            return []
-        } else if (amount > count){
-            amount = count
-        }
-        
-        let lowerRange = Int.random(in: 0...count-amount)
-        let upperRange = lowerRange + amount
-        
-        let questions = try await GrammarQuestion.query(on: req.db).range(lower: lowerRange, upper: upperRange-1).all()
+        let questions = try await GrammarQuestion.query(on: req.db).all().randomSample(count: amount)
         
         return questions.map({ question in
             GrammarQuestionDTO(question: question.question, correct_answer: question.content, meaning: question.meaning, topic: question.topic, level: question.level)
